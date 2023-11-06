49425bb0fc9062fcbd63a7cdc5c10879239ac635
fredyquevedo1990 <149911399+fredyquevedo1990@users.noreply.github.com>
2023-11-05 23:25:16 -0500

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("lado del cuadrado: " );
+        double lado = scanner.nextDouble();
+
+        System.out.print("lado del cuadrado: ");
+        double area = scanner.nextDouble();
+        double area = (lado * lado);
+
+
+        System.out.print(perimetro + " "+"el area es: "+ area +" "+"area.");
+    }
+}
